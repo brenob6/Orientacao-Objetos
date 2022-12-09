0db7ff7672620da5918ed7053b051cd0cb76575f
Breno Queiroz Lima <brenob6@gmail.com>
2022-12-09 20:59:08 -0300

    Add Stickers DC and Copa

---
+++ OO-Project/src/br/unb/model/StickerCopa.java
@@ -0,0 +1,38 @@
+package br.unb.model;
+
+public class StickerCopa extends Sticker{
+
+    private String selecao;
+    private String posicao;
+    
+
+    public String getSelecao() {
+        return selecao;
+    }
+
+
+    public void setSelecao(String selecao) {
+        this.selecao = selecao;
+    }
+
+
+    public String getPosicao() {
+        return posicao;
+    }
+
+
+    public void setPosicao(String posicao) {
+        this.posicao = posicao;
+    }
+
+
+    public StickerCopa(String name, String raridade, int quant, String UrlImage,
+            String selecao, String posicao) {
+        super(name, raridade, quant, UrlImage);
+        // TODO Auto-generated constructor stub
+        this.selecao = selecao;
+        this.posicao = posicao;
+
+    }
+
+}
---
+++ OO-Project/src/br/unb/model/StickerDC.java
@@ -0,0 +1,35 @@
+package br.unb.model;
+
+public class StickerDC extends Sticker{
+
+    private String movie;
+    private String alias;
+
+    public StickerDC() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public StickerDC(String name, String raridade, int quant, String UrlImage,
+            String movie, String alias) {
+        super(name, raridade, quant, UrlImage);
+        // TODO Auto-generated constructor stub
+        this.movie = movie;
+        this.alias = alias;
+    }
+
+    public String getMovie() {
+        return movie;
+    }
+    public void setMovie(String movie) {
+        this.movie = movie;
+    }
+    public String getAlias() {
+        return alias;
+    }
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    
+}
