583c04716616153e8a56566ee009cda5f686bf09
Breno Queiroz Lima <brenob6@gmail.com>
2022-12-09 20:15:13 -0300

    Add methods in Sticker class

--- OO-Project/src/br/unb/model/Sticker.java
+++ OO-Project/src/br/unb/model/Sticker.java
@@ -1,20 +1,67 @@
 package br.unb.model;
 
-public class Sticker {
+import java.net.MalformedURLException;
+import java.net.URL;
 
-    public int getId() {
-        return id;
+import javax.swing.ImageIcon;
+
+public abstract class Sticker {
+
+    private String raridade;
+    private int quant;
+    private String UrlImage;
+    private String name;
+    
+    public ImageIcon getImage() {
+
+        URL url = null; 
+        try {
+            url = new URL(UrlImage);
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        return new ImageIcon(url);
     }
-    public void setId(int id) {
-        this.id = id;
+    
+    public String getName() {
+        return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRaridade() {
+        return raridade;
+    }
+
+    public void setRaridade(String raridade) {
+        this.raridade = raridade;
+    }
+
     public int getQuant() {
         return quant;
     }
+
     public void setQuant(int quant) {
         this.quant = quant;
     }
-    private int id;
-    private int quant;
+
+    public String getUrlImage() {
+        return UrlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        UrlImage = urlImage;
+    }
+    
+    public Sticker(String name, String raridade, int quant, String UrlImage) {
+        this.name = name;
+        this.raridade = raridade;
+        this.quant = quant; 
+        this.UrlImage = UrlImage;
+    }
 
 }
