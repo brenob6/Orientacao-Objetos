b97f56c367b803a723d182321a03c9789a1cc20f
Breno Queiroz Lima <brenob6@gmail.com>
2022-12-09 19:46:07 -0300

    Add methods in User Class

--- OO-Project/src/br/unb/model/User.java
+++ OO-Project/src/br/unb/model/User.java
@@ -7,6 +7,18 @@ public class User {
     
     public User() {
         this.albuns = new ArrayList<Album>();
+    } 
+    
+    public void addAlbum(Album album) {
+        this.albuns.add(album);
     }
-
+    
+    public void deleteAlbum(int id) {
+        for (int i = 0; i < albuns.size(); i++) {
+            if(albuns.get(i).getId() == id) {
+                albuns.remove(i);
+                break;
+            }
+        }
+    }
 }
