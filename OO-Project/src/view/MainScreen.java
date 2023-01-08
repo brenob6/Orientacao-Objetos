474170a96e71640f219aa5679ef873e1beddced4
Breno Queiroz Lima <brenob6@gmail.com>
2023-01-08 14:20:12 -0300

    add mouseEvent

--- OO-Project/src/view/MainScreen.java
+++ OO-Project/src/view/MainScreen.java
@@ -4,7 +4,16 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+
+import model.Album;
+
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import java.util.ArrayList;
 
 public class MainScreen extends JFrame {
       
@@ -13,10 +22,11 @@ public class MainScreen extends JFrame {
 
     private final String title = "Pseudo Album";
 
+    private ArrayList<JPanel> albunsPanel = new ArrayList<JPanel>();
+
     public MainScreen() {
         
         titlePanel(); 
-        albumListPanel();
         screenConfig();
     }
 
@@ -27,11 +37,38 @@ public class MainScreen extends JFrame {
         this.add(titlePanel);
     }
 
-    private void albumListPanel() {
+    public void updateAlbumListPanel() {
         JPanel panel = new JPanel();
         panel.setBounds(0, 100, WIDTH, 700);
+        panel.setLayout(new GridLayout(9, 1, 10, 10));
         panel.setBackground(Color.BLUE);
+
+        albunsPanel.forEach((albumPanel) -> {
+            panel.add(albumPanel);
+        });
+
         this.add(panel);
+    }
+
+    public void createAlbumCard(Album album) {
+        JPanel panel = new JPanel();
+        panel.setBounds(0, 0, WIDTH, HEIGHT/6);
+        panel.setBackground(Color.GREEN);
+        panel.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                System.out.println(album.getAlias());
+            }
+        });
+
+        JLabel label = new JLabel();
+        label.setBounds(0, 0, WIDTH, HEIGHT/6);
+        label.setFont(new Font("Arial", Font.BOLD, 20));
+        label.setText(album.getName());
+
+        panel.add(label);
+        albunsPanel.add(panel);
+
+        updateAlbumListPanel();
     }
 
     private void screenConfig() {
