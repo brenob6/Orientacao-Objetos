6e001fd1cb00b136565ad57e49fe3d0e44745d4d
Breno Queiroz Lima <brenob6@gmail.com>
2023-01-10 11:43:21 -0300

    add TabbedPane

--- OO-Project/src/view/StickerScreen.java
+++ OO-Project/src/view/StickerScreen.java
@@ -2,65 +2,109 @@ package view;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.border.Border;
 
 import model.Album;
 import model.Sticker;
 
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-public class StickerScreen extends JFrame{
+public class StickerScreen extends JFrame implements Config{
 
     private JPanel topMenuPane = new JPanel();
     private JPanel listStickesPanel = new JPanel();
-    private JScrollPane scrollPane;
+    private JPanel listRepeatedPanel = new JPanel();
+    private JPanel listMissingPanel = new JPanel();
+
+    private JTabbedPane tabPane = new JTabbedPane();
 
     public StickerScreen(Album album) {
         this.setTitle("Pseudo Album");
-        this.setSize(800, 600);
+        this.setSize(Config.FRAME_WIDTH, Config.FRAME_HEIGHT);
+        this.setBackground(Config.BACKGROUND_COLOR);
+        this.setLayout(new BorderLayout());
         this.setResizable(true);
-        this.setLayout(new GridLayout(album.getTotal(), 1, 10 , 10));
-        this.setVisible(true);
-        
-        listStickesPanel.setLayout(new GridLayout(album.getTotal(), 1, 5, 5));
+
+        this.add(tabPane);
+        listStickesPanel.setBackground(Config.BACKGROUND_COLOR);
+
+        titlePanel(album.getName());
+
+        tabPane.add("TODAS", listStickesPanel);
+        tabPane.add("FALTANTES", listMissingPanel);
+        tabPane.add("REPETIDAS", listRepeatedPanel);
+
+        //scrollPane = new JScrollPane(
+        //    listStickesPanel,
+        //    JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+        //    JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED
+        //);
+
+        //this.add(scrollPane, BorderLayout.CENTER);
 
         loadStickerCards(album);
+        //this.add(listStickesPanel, BorderLayout.CENTER);
 
-        scrollPane = new JScrollPane(
-            listStickesPanel,
-            JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-            JScrollPane.HORIZONTAL_SCROLLBAR_NEVER
-        );
-        scrollPane.setBounds(0, 100, 800, 700);
+        this.setVisible(true);
+    }
 
-        this.add(scrollPane);
+    private void titlePanel(String title) {
+        topMenuPane.setSize(800, 100);
+        topMenuPane.setBackground(Config.TITLE_COLOR);
+        topMenuPane.setVisible(true);
+
+        this.add(topMenuPane, BorderLayout.NORTH);
     }
 
     private void loadStickerCards(Album album) {
         album.getStickers().forEach(sticker -> {
-            createStickerCard(sticker);
+            listStickesPanel.add(makeStickerCard(sticker));
+        });
+
+        album.getRepeatedStickers().forEach(sticker -> {
+            listRepeatedPanel.add(makeStickerCard(sticker));
+        });
+
+        album.getMissingStickers().forEach(sticker -> {
+            listMissingPanel.add(makeStickerCard(sticker));
         });
     }
 
-    public void createStickerCard(Sticker sticker) {
+    public JPanel makeStickerCard(Sticker sticker) {
 
         JPanel card = new JPanel();
-        card.setBackground(Color.RED);
-        card.setSize(800,100);
+        card.setPreferredSize(new Dimension(800, 80));
     
         JLabel countLabel = new JLabel("0");
-        JButton addButton = new JButton("+");
-        JButton removeButton = new JButton("-");
+        countLabel.setFont(new Font("arial", Font.BOLD, 20));
 
+        JButton addButton = new JButton("+");
+        addButton.setPreferredSize(new Dimension(50, 50));
+        JButton removeButton = new JButton("-");
+        removeButton.setPreferredSize(new Dimension(50, 50));
+    
+        JCheckBox checkBox = new JCheckBox(); 
+        checkBox.setPreferredSize(new Dimension(80, 80));
+    
+        
+        card.add(checkBox);
         card.add(addButton);
         card.add(countLabel);
         card.add(removeButton);
 
-        listStickesPanel.add(card);
+        return card;
     }
 }
