694d908efdfe7c49d3ae4077719d2175ecc8fe08
Breno Queiroz Lima <brenob6@gmail.com>
2023-01-14 11:20:20 -0300

    new constants

--- OO-Project/src/view/Config.java
+++ OO-Project/src/view/Config.java
@@ -2,6 +2,7 @@ package view;
 
 import java.awt.Font;
 import java.awt.Color;
+import java.awt.Dimension;
 
 public interface Config {
     int FRAME_WIDTH = 800;
@@ -10,6 +11,9 @@ public interface Config {
     int TITLE_HEIGHT = 100;
 
     Font FONT = new Font("monospaced", Font.BOLD, 30);
+
+    Dimension buttonDimension = new Dimension(50, 50);
+    Dimension carDimension = new Dimension(800, 80);
 
     Color BACKGROUND_COLOR = new Color(0xCFFDE1);
     Color COLOR_WHITE = new Color(0xE5E5CB);
