1da51101e5b7711b758f2a0b3968c711d40288ed
Breno Queiroz Lima <brenob6@gmail.com>
2023-01-13 21:02:50 -0300

    better style

--- OO-Project/src/view/MainScreen.java
+++ OO-Project/src/view/MainScreen.java
@@ -1,5 +1,6 @@
 package view;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -7,25 +8,21 @@ import javax.swing.JPanel;
 
 import model.Album;
 
-import java.awt.Color;
-import java.awt.Font;
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import java.util.ArrayList;
 
-public class MainScreen extends JFrame {
+public class MainScreen extends JFrame implements Config{
       
     private final int WIDTH = 800;
     private final int HEIGHT = 600;
-    private final Font FONT = new Font("Arial", Font.BOLD, 20);
 
     private final String title = "Pseudo Album";
 
     private JPanel albunsListPanel;
-    
-    //private ArrayList<JPanel> albunsPanel = new ArrayList<JPanel>();
 
     public MainScreen() {
 
@@ -43,14 +40,18 @@ public class MainScreen extends JFrame {
     private void titlePanel() {
         JLabel label = new JLabel();
         label.setText("Controle de Figurinhas");
-        label.setFont(FONT);
+        label.setFont(Config.FONT);
+        label.setForeground(Config.COLOR_WHITE);
 
-        JButton button = new JButton();
-        button.setFont(FONT);
-        button.setText("+");
+        JButton button = new JButton("+");
+        button.setBorder(BorderFactory.createEmptyBorder());
+        button.setBackground(Config.COLOR_BLACK);
+        button.setForeground(Config.COLOR_WHITE);
+        button.setPreferredSize(Config.buttonDimension);
+        button.setFont(Config.FONT);
 
         JPanel titlePanel = new JPanel();
-        titlePanel.setBackground(Color.RED);
+        titlePanel.setBackground(Config.COLOR_BLACK);
         titlePanel.setBounds(0, 0, WIDTH, 50);
         titlePanel.add(label);
         titlePanel.add(button);
@@ -58,41 +59,34 @@ public class MainScreen extends JFrame {
         this.add(titlePanel);
     }
 
-    //public void updateAlbumListPanel() {
-    //    albunsPanel.forEach((albumPanel) -> {
-    //        albunsListPanel.add(albumPanel);
-    //    });
-    //}
-
-    public void createAlbumCard(Album album) {
+    public void makeAlbumCard(Album album) {
         JPanel panel = new JPanel();
         panel.setBounds(0, 0, WIDTH, HEIGHT/6);
-        panel.setBackground(Color.GREEN);
+        panel.setBackground(Config.COLOR_WHITE);
         panel.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
-                new StickerScreen();
+                new StickerScreen(album);
             }
         });
 
         JLabel label = new JLabel();
         label.setBounds(0, 0, WIDTH, HEIGHT/6);
-        label.setFont(new Font("Arial", Font.BOLD, 20));
+        label.setFont(Config.FONT);
         label.setText(album.getName());
 
         panel.add(label);
-
+        
         albunsListPanel.add(panel);
-       // albunsPanel.add(panel);
-
-        //updateAlbumListPanel();
     }
 
-    public void removeAlbumCard(Album album) {
+    private void removeAlbumCard(JPanel card) {
+        albunsListPanel.remove(card);
     }
 
     private void screenConfig() {
         this.setTitle(title);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout());
         this.setSize(WIDTH, HEIGHT);
         this.setResizable(false);
         this.setLayout(null);
