f26ae8186edfddcc617122f00d57cfd3e1fa45fc
Breno Queiroz Lima <brenob6@gmail.com>
2023-01-24 00:37:45 -0300

    add new components

--- OO-Project/src/view/Componets.java
+++ OO-Project/src/view/Componets.java
@@ -2,33 +2,38 @@ package view;
 
 import javax.swing.*;
 
-import model.Album;
+import java.awt.*;
 
 public class Componets implements Config{
-    protected static JButton makeButton(String text) {
+
+    protected static JPanel card() {
+
+        JPanel card = new JPanel();
+        //card.setPreferredSize(Config.cardDimension);
+        card.setMaximumSize(cardDimension);
+        card.setMinimumSize(cardDimension);
+        card.setBackground(Config.COLOR_WHITE);
+
+        return card;
+    }
+
+    protected static JLabel label (String text) {
+        JLabel label = new JLabel(); 
+        label.setFont(Config.FONT);
+        label.setText(text);
+        label.setBackground(Config.COLOR_WHITE);
+        return label;
+    }
+
+    protected static JButton makeButton(String text, Color background) {
 
         JButton button = new JButton(text);
         button.setBorder(BorderFactory.createEmptyBorder());
-        button.setBackground(COLOR_BLACK);
+        button.setBackground(background);
         button.setForeground(COLOR_WHITE);
         button.setPreferredSize(buttonDimension);
         button.setFont(FONT);
 
         return button;
-    }
-    public static JPanel makeStickerCard(Album album) {
-
-        JPanel card = new JPanel();
-        card.setPreferredSize(Config.cardDimension);
-        card.setBackground(Config.COLOR_WHITE);
-
-        JLabel nameLabel = new JLabel(); 
-        nameLabel.setFont(Config.FONT);
-        nameLabel.setText(album.getName());
-        nameLabel.setBackground(Config.COLOR_WHITE);
-        
-        card.add(nameLabel);
-
-        return card;
     }
 }
