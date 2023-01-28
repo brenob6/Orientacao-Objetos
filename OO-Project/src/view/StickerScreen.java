8770a2f19d9cddce47a1c8c779c1380f7620dd2d
TheCarlosRamos <116928666+TheCarlosRamos@users.noreply.github.com>
2023-01-27 22:37:47 -0300

    Update StickerScreen.java

--- OO-Project/src/view/StickerScreen.java
+++ OO-Project/src/view/StickerScreen.java
@@ -26,7 +26,11 @@ import java.awt.event.InputMethodListener;
 import java.awt.event.InputMethodEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-
+/**
+ * Class responsible for implementing the interface Config, inherits the JFRame class and makes possible the manufacture of the sticker screen 
+ * so that the user can interact with the album through the actions provided by the cards, buttons and its events.
+ *
+ */
 public class StickerScreen extends JFrame implements Config{
 
     private JPanel topMenuPane = new JPanel();
@@ -41,7 +45,10 @@ public class StickerScreen extends JFrame implements Config{
     private JTabbedPane tabPane = new JTabbedPane();
 
     private Album album;
-
+/**
+ * Constructor responsible for instantiate the class StickerScreen
+ * @param album
+ */
     public StickerScreen(Album album) {
         this.album = album;
         this.setTitle("Pseudo Album");
@@ -80,7 +87,12 @@ public class StickerScreen extends JFrame implements Config{
 
         this.setVisible(true);
     }
-
+/**
+ * Method responsible for creating a title panel according to its title and also 
+ * its components and their following actions  
+ * @param title
+ * @return topMenuPane
+ */
     private JPanel titlePanel(String title) {
         topMenuPane.setSize(800, 100);
         topMenuPane.setBackground(Config.COLOR_BLACK);
@@ -143,7 +155,11 @@ public class StickerScreen extends JFrame implements Config{
         return topMenuPane;
         //this.add(topMenuPane, BorderLayout.NORTH);
     }
-
+/**
+ * Method responsible for loading and updating the data that is contained 
+ * in the list of stickers, missing stickers and repeated stickers  
+ * @param album
+ */
     private void loadStickerCards(Album album) {
         listStickesPanel.removeAll();
         listRepeatedPanel.removeAll();
@@ -169,7 +185,11 @@ public class StickerScreen extends JFrame implements Config{
         listRepeatedPanel.repaint();
         
     }
-
+/**
+ * Method responsible for creating a sticker card 
+ * @param sticker
+ * @return
+ */
     public JPanel makeStickerCard(Sticker sticker) {
 
         JPanel card = Componets.card();
@@ -228,7 +248,11 @@ public class StickerScreen extends JFrame implements Config{
 
         return card;
     }
-
+/**
+ * Method responsible for creating a panel that gives the information about the sticker 
+ * @param sticker
+ * @return panel
+ */
     private JPanel makeInfoPanel(Sticker sticker) {
         JPanel panel = new JPanel();
         JLabel img = new JLabel(); 
@@ -237,7 +261,11 @@ public class StickerScreen extends JFrame implements Config{
         panel.add(img);
         return panel;
     }
-
+/**
+ * Method responsible for generating an organized and visible text in more than one line that contains the sticker's information  
+ * @param sticker
+ * @return text
+ */
     private String makeInfoText(Sticker sticker) {
         String[] infos = sticker.toString().split(";");
         String text = "<html>";
