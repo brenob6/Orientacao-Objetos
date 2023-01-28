5e17077c1a66b55b96af5681ee68a65dd33f006e
TheCarlosRamos <116928666+TheCarlosRamos@users.noreply.github.com>
2023-01-27 22:30:48 -0300

    Update LoadData.java

--- OO-Project/src/controller/LoadData.java
+++ OO-Project/src/controller/LoadData.java
@@ -7,6 +7,10 @@ import model.CupSticker;
 import model.DcSticker;
 import model.Sticker;
 
+/**
+ * Class responsible for simulating the database of both Cup and DC stickers that exists in an album
+ *
+ */
 public class LoadData {
 
     // Dados figuras DC
@@ -29,7 +33,10 @@ public class LoadData {
     };
 
     private static String DC = "Heróis DC";
-
+/**
+ * Method responsible for creating a list of specific types of albums with its following stickers 
+ * @return albumList
+ */
     public static ArrayList<Album> getAlbums() {
 
         ArrayList<Album> albumList = new ArrayList<>();
@@ -47,6 +54,11 @@ public class LoadData {
         return albumList;
     }
 
+    /**
+     * Method responsible for creating stickers with its informations
+     * @param n
+     * @return stickerList
+     */
     private static ArrayList<Sticker> makeStickers(String n) {
         
         ArrayList<Sticker> stickerList = new ArrayList<>();
