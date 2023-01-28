d5db36102bc777f7521357adc61c988aeda4dc90
TheCarlosRamos <116928666+TheCarlosRamos@users.noreply.github.com>
2023-01-27 22:32:57 -0300

    Update Album.java

--- OO-Project/src/model/Album.java
+++ OO-Project/src/model/Album.java
@@ -1,7 +1,10 @@
 package model;
 
 import java.util.ArrayList;
-
+/**
+ * Class responsible for simulating an album
+ *
+ */
 public class Album {
     
     private String name;
@@ -15,7 +18,10 @@ public class Album {
     public void setStickers(ArrayList<Sticker> stickers) {
         this.stickers = stickers;
     }
-    
+    /**
+     * Method responsible for searching and listing repeated stickers in an album
+     * @return repeated
+     */
     public ArrayList<Sticker> getRepeatedStickers(){
         ArrayList<Sticker> repeated = new ArrayList<Sticker>();
         stickers.forEach((sticker) -> {
@@ -24,7 +30,10 @@ public class Album {
         });
         return repeated;
     }
-    
+    /**
+     * Method responsible for searching and listing missing stickers in an album
+     * @return missing
+     */
     public ArrayList<Sticker> getMissingStickers() {
         ArrayList<Sticker> missing = new ArrayList<Sticker>();
         stickers.forEach((sticker) -> {
@@ -34,7 +43,12 @@ public class Album {
         return missing;
         
     }
-
+/**
+ * Constructor responsible for instantiate the class Album
+ * @param name
+ * @param alias
+ * @param stickers
+ */
     public Album(String name, String alias, ArrayList<Sticker> stickers) {
         this.name = name;
         this.alias = alias;
