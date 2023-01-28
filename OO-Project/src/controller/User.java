6556f87f2f00dbad52873b450de4013ae13e9e5b
TheCarlosRamos <116928666+TheCarlosRamos@users.noreply.github.com>
2023-01-28 20:42:57 -0300

    Update User.java

--- OO-Project/src/controller/User.java
+++ OO-Project/src/controller/User.java
@@ -5,7 +5,8 @@ import model.Album;
 import model.Sticker;
 
 /**
- * Class User simulates a behavior of an user that has an album and is a holder of it 
+ * Class User simulates a behavior of an user 
+ * that has an album and is a holder of it 
  *
  */
 public class User {
@@ -16,10 +17,10 @@ public class User {
         this.albuns = new ArrayList<>();
     }
 
-    /**
-     * Method that is responsible for add an album to the user
-     * @param album
-     */
+/**
+ * Method that is responsible for add an album to the user
+ * @param album
+ */
     public void addAlbum(Album album) {
         albuns.add(album);
     }
@@ -28,14 +29,19 @@ public class User {
         return albuns;
     }
     
-    /**
-     * Method responsible for removing an album in the list of user's albums
-     * @param album
-     */
+/**
+ * Method responsible for removing an album in the list of user's albums
+ * @param album
+ */
     public void removeAlbum (Album album) {
         albuns.remove(album);
     }
     
+/**
+ * Method responsible for creating an album given its name
+ * @param name
+ * @return album
+ */
     public Album createAlbum (String name){
 
         Album album = new Album();
@@ -46,14 +52,13 @@ public class User {
 
     
     /**
-     * Method responsible for searching for an album given its name so that the album can be find. 
-     * The searching will be based on the data of the holder according to its album.
-     * The album will be found if the name of the album exists in the list of the user, then, 
-     * the method will return the name of the album. Otherwise, it will return null. 
-     * @param name
-     * @return album
-     * @return null
-     */
+ * Method responsible for searching for an album given its name so that the album can be find. 
+ * The searching will be based on the data of the holder according to its album.
+ * The album will be found if the name of the album exists in the list of the user, then, 
+ * the method will return the name of the album. Otherwise, it will return null. 
+ * @param name
+ * @return album
+ */
     //public Album findAlbum (String name){
     //    for (Album album : holder) {
     //        if(album.getName().equals(name))
@@ -61,10 +66,10 @@ public class User {
     //    }
     //    return null;
     //}
-    /**
-     * Method responsible for listing all the albums that already exists.
-     * It is going to print their names on the screen. 
-     */
+/**
+ * Method responsible for listing all the albums that already exists.
+ * It is going to print their names on the screen. 
+ */
    public void listAll() {
        albuns.forEach((album) -> System.out.println(album.getName()));
    }
